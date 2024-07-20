574a89c492e903994ef4cd1373099453cbbb22e7
Elmira <gilfanova-03@inbox.ru>
2024-07-20 20:40:54 +0300

    add constructor

--- s21_matrix_oop.cpp
+++ s21_matrix_oop.cpp
@@ -3,3 +3,69 @@
 
 S21Matrix::S21Matrix() : rows_(0), cols_(0), matrix_(nullptr) {
 }
+
+//  сделать проверку на меньше 0
+S21Matrix::S21Matrix(int rows, int cols) : rows_(rows), cols_(cols){
+    AllocateMemory();
+}
+
+S21Matrix::S21Matrix(const S21Matrix& other) : rows_(other.rows_), cols_(other.cols_) {
+    AllocateMemory();
+    CopyMatrix(other);
+}
+
+S21Matrix::S21Matrix(S21Matrix&& other) : rows_(other.rows_), cols_(other.cols_), matrix_(other.matrix_) {
+    other.matrix_ = nullptr;
+    other.rows_ = 0;
+    other.cols_ = 0;
+}
+
+S21Matrix::~S21Matrix() {
+    DeallocateMemory();
+}
+
+void S21Matrix::AllocateMemory() {
+    matrix_ = new double*[rows_];
+    for (int i = 0; i < rows_; ++i) {
+        matrix_[i] = new double[cols_]();
+    }
+}
+
+void S21Matrix::DeallocateMemory() {
+    matrix_ = new double*[rows_];
+    for (int i = 0; i < rows_; ++i) {
+        matrix_[i] = new double[cols_]();
+    }
+}
+
+void S21Matrix::DeallocateMemory() {
+    if(matrix_){
+        for(int i=0;i<rows_;i++){
+            delete[] matrix_[i];
+        }
+        delete[] matrix_;
+        matrix_ = nullptr;
+    }
+}
+
+void S21Matrix::MoveMatrix(S21Matrix&& other)  {
+    DeallocateMemory();
+    rows_ = other.rows_;
+    cols_ = other.cols_;
+    matrix_ = other.matrix_;
+    other.matrix_ = nullptr;
+    other.rows_ = 0;
+    other.cols_ = 0;
+}
+
+void S21Matrix::CopyMatrix(const S21Matrix& other) {
+    for (int i = 0; i < rows_; ++i) {
+        std::copy(other.matrix_[i], other.matrix_[i] + cols_, matrix_[i]);
+    }
+}
+
+
+
+
+
+
--- s21_matrix_oop.h
+++ s21_matrix_oop.h
@@ -1,29 +1,58 @@
 #ifndef S21_MATRIX_OOP_H
 #define S21_MATRIX_OOP_H
 
-class s21matrix{
+class S21Matrix{
     private:
       int rows_;
       int cols_;
       double **matrix_;
+
+      AllocateMemory();
+      DeallocateMemory();
+      CopyMatrix();
+      MoveMatrix();
+
     public:
-       S21Matrix();
-       S21Matrix(int rows, int cols);
-       S21Matrix(const S21Matrix& other);
-       S21Matrix(S21Matrix&& other);
-       ~S21Matrix();
+
+        int getRows() { return rows_; }
+        int getCols() { return cols_; }
+        double** getMatrix() { return matrix_; }
+        void SetRows(int rows);
+        void SetColumns(int cols);
 
 
-       bool EqMatrix(const S21Matrix& other);
-       void SumMatrix(const S21Matrix& other);
-       void SubMatrix(const S21Matrix& other);
-       void MulNumber(const double num);
-       void MulMatrix(const S21Matrix& other);
-       S21Matrix Transpose();
-       S21Matrix CalcComplements();
-       double Determinant();
-       S21Matrix InverseMatrix();
 
+        S21Matrix();
+        S21Matrix(int rows, int cols);
+        S21Matrix(const S21Matrix& other);
+        S21Matrix(S21Matrix&& other);
+        ~S21Matrix();
+
+        bool EqMatrix(const S21Matrix& other);
+        void SumMatrix(const S21Matrix& other);
+        void SubMatrix(const S21Matrix& other);
+        void MulNumber(const double num);
+        void MulMatrix(const S21Matrix& other);
+        S21Matrix Transpose();
+        S21Matrix CalcComplements();
+        double Determinant();
+        S21Matrix InverseMatrix();
+
+        S21Matrix operator+=(const S21Matrix& other);
+        S21Matrix operator-=(const S21Matrix& other);
+        S21Matrix operator*=(const S21Matrix& other);
+        S21Matrix operator*=(const double num);
+
+        S21Matrix operator+(const S21Matrix& other);
+        S21Matrix operator-(const S21Matrix& other);
+        S21Matrix operator*(const S21Matrix& other);
+        S21Matrix operator*(const double num);
+
+        S21Matrix& operator=(const S21Matrix& other);
+        bool operator==(const S21Matrix& other);
+
+        double& operator()(int i, int j);
+        double& operator()(int i, int j) const;
 
 
 
